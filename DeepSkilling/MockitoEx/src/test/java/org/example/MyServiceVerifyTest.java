e4a93cc3e5e6d1f3dceb9c800ccaf7ef601de7aa
Vepada Yamuna <yamunav32006@gmail.com>
2026-07-07 22:10:53 +0530

    mokito excercise error resovled

---
+++ DeepSkilling/MockitoEx/src/test/java/org/example/MyServiceVerifyTest.java
@@ -0,0 +1,18 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.verify;
+
+public class MyServiceVerifyTest {     // <-- must say MyServiceVerifyTest here
+
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
